1302dab4966be5736d32c5beff24a86d531846e1
Abdulrahman-devx7 <rafiomaster977@gmail.com>
2026-08-21 01:41:25 +0300

    refactor: abstract repeated logic in transaction functions

--- Bank-Management-System.cpp
+++ Bank-Management-System.cpp
@@ -1374,11 +1374,9 @@ void ShowTransactionsMenu()
     cout << UI_LINE_BOUNDS << "\n";
 }
 
-void DepositByAccNumber(unordered_map<string, stClientData>& clients, string fileName)
+void GetClientForTransaction(unordered_map<string, stClientData>::iterator clientIt, unordered_map<string, stClientData>& clients)
 {
     string accountNumber = "";
-
-    unordered_map<string, stClientData>::iterator clientIt;
     do
     {
         accountNumber = readAccountNumber();
@@ -1388,6 +1386,13 @@ void DepositByAccNumber(unordered_map<string, stClientData>& clients, string fil
             cout << "\nThe client with the account number[" << accountNumber << "] doesn't exist. Enter a different account number: ";
 
     } while (clientIt == clients.end());
+}
+
+void DepositByAccNumber(unordered_map<string, stClientData>& clients, string fileName)
+{
+    unordered_map<string, stClientData>::iterator clientIt;
+
+    GetClientForTransaction(clientIt, clients);
 
     cout << "\nThe following are the client data: \n\n";
     PrintInfoCard(clientIt->second);
